e01915e368b6af29b49cad24d8a921f1ace59360
hyunjinSD <hyenjin09@nate.com>
2024-08-31 19:55:54 +0900

    java,variable study

---
+++ src/chap02/Question.java
@@ -0,0 +1,10 @@
+package chap02;
+
+public class Question {
+    public static void main(String args[]) {
+        // 에러가 발생하는 부분을 고쳐보세요.
+        int a = 999;
+        char b = 'a';
+        float c = 1.23F;
+    }
+}
