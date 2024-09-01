83e09e7886833fa4889feb3a0cef5a1d311ab35f
hyunjinSD <hyenjin09@nate.com>
2024-09-01 18:09:59 +0900

    java,variable study

---
+++ src/chap02/VariableQuiz.java
@@ -0,0 +1,19 @@
+package chap02;
+
+public class VariableQuiz {
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 20;
+        int z = 300;
+
+        // 코드를 작성해주세요
+        x = y;
+        y = z;
+        z = 1;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+    }
+}
