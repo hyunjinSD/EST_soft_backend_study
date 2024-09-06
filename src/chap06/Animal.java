c385e3986ce7606a8f644a5b82e6f3424d517bdb
hyunjinSD <hyenjin09@nate.com>
2024-09-07 00:03:35 +0900

    Class study

---
+++ src/chap06/Animal.java
@@ -0,0 +1,17 @@
+package chap06;
+
+public class Animal {
+    String name;
+    String sleep;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String sleep() {
+        return sleep;
+    }
+}
