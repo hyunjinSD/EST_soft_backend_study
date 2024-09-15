54399c440bd4c8b0e7abfabec4d55a84cd44a14a
hyunjinSD <hyenjin09@nate.com>
2024-09-15 22:19:01 +0900

    generic study

---
+++ src/chap10/generic/Box.java
@@ -0,0 +1,13 @@
+package chap10.generic;
+
+public class Box<T> {
+    private T object;
+
+    public void set(T object) {
+        this.object = object;
+    }
+
+    public T get() {
+        return this.object;
+    }
+}
