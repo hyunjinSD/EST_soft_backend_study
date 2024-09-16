d6d25e445e97cec4db02c30a9d2dce93effd3441
hyunjinSD <hyenjin09@nate.com>
2024-09-16 22:13:57 +0900

    generic study

---
+++ src/chap10/BoxExample.java
@@ -0,0 +1,12 @@
+package chap10;
+
+public class BoxExample {
+    public static void main(String[] args) {
+        Box box = new Box();
+        box.set("문자열");
+
+        String str = (String) box.get();
+        System.out.println(str);
+
+    }
+}
