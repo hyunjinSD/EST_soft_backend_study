fb64435e36fbda7b6b0364632713cc777cf6c4f7
hyunjinSD <hyenjin09@nate.com>
2024-09-24 20:04:40 +0900

    Lambda study

---
+++ src/chap12/ConstructorReferencesExample.java
@@ -0,0 +1,15 @@
+package chap12;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class ConstructorReferencesExample {
+    public static void main(String[] args) {
+        Function<String, Member> function = Member::new;
+        function.apply("string");
+
+
+        BiFunction<String, String, Member> function2 = Member::new;
+        function2.apply("string", "string2");
+    }
+}
---
+++ src/chap12/MethodReferencesExample.java
@@ -0,0 +1,20 @@
+package chap12;
+
+import java.util.function.IntBinaryOperator;
+
+public class MethodReferencesExample {
+    public static void main(String[] args) {
+        IntBinaryOperator operator;
+
+        // 정적 메소드 호출
+        operator = (left, right) -> Calculator.staticMethod(left, right);
+        System.out.println(operator.applyAsInt(1,2));
+
+        operator = Calculator::staticMethod;
+
+        // 인스턴스 메소드 호출
+        Calculator calculator = new Calculator();
+        operator = calculator::method;
+        System.out.println(operator.applyAsInt(3,5));
+    }
+}
