a768094644a228485914fb61fcb75d877ea42b3d
hyunjinSD <hyenjin09@nate.com>
2024-10-03 19:14:11 +0900

    Stream study

---
+++ src/chap13/SortedExample.java
@@ -0,0 +1,27 @@
+package chap13;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 정렬
+ */
+
+public class SortedExample {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("e", "a", "c", "z", "d");
+
+        List<String> sortedList = list.stream()
+                .sorted()            // 오름차순 정렬
+                .toList();
+        System.out.println(sortedList);
+
+        List<String> sortedList2 = list.stream()
+                .sorted(Comparator.reverseOrder())
+                .toList();
+        System.out.println(sortedList2);  // 내림차순 정렬
+
+    }
+}
